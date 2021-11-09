cc4e7cef03a25eae4e70b716617f7b5f8af61e39
YBYuan007 <yuanyanbin2011@gmail.com>
2021-11-09 17:41:14 +0000

    add employee table seeds

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -18,3 +18,9 @@ puts "----------"
 # Add the following code directly inside the Employee model (class): belongs_to :store
 # Add some data into employees. Here's an example of one (note how it differs from how you create stores): @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
 # Go ahead and create some more employees using the create method. You can do this by making multiple calls to create (like you have before.) No need to assign the employees to variables though. Create them through the @store# instance variables that you defined in previous exercises. Create a bunch under @store1 (Burnaby) and @store2 (Richmond). Eg: @store1.employees.create(...).
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Jessica", last_name: "Mimi", hourly_rate: 100)
+@store1.employees.create(first_name: "Tommy", last_name: "Tom", hourly_rate: 20)
+@store2.employees.create(first_name: "Yma", last_name: "Ha", hourly_rate: 30)
+@store2.employees.create(first_name: "Alice", last_name: "Zhang", hourly_rate: 23)
